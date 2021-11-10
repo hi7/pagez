88ce86cefedcccc63740de61cba1a6e32a25279c
Hilger Baumstark <hilger.zone@gmail.com>
2021-11-10 20:40:18 +0000

    clear bitmap

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const fs = std.fs;
+const Vector = std.meta.Vector;
 
 pub fn main() anyerror!void {
     var buffer: [1366*768*4]u8 = undefined;
@@ -8,8 +9,10 @@ pub fn main() anyerror!void {
 
     var bitmap = try allocator.alloc(u8, 1366*768*4);
     defer allocator.free(bitmap);
+
+    clear(bitmap);
     
-    const white = [4]u8{ 255, 255, 255, 255 };
+    const white: Vector(4, u8) = .{ 255, 255, 255, 255 };
     pixel(white, bitmap, 0);
     const blue = [4]u8{ 255, 0, 0, 255 };
     pixel(blue, bitmap, 1);
@@ -21,7 +24,7 @@ pub fn main() anyerror!void {
     try flush(bitmap, fb);
 }
 
-fn pixel(color: [4]u8, bitmap: []u8, pos: u16) void {
+fn pixel(color: Vector(4,u8), bitmap: []u8, pos: u16) void {
    bitmap[pos] = color[0];
    bitmap[pos+1] = color[1];
    bitmap[pos+2] = color[2];
@@ -30,6 +33,12 @@ fn pixel(color: [4]u8, bitmap: []u8, pos: u16) void {
 
 fn flush(bitmap: []u8, fb: fs.File) fs.File.PWriteError!void {
    _ = try fb.write(bitmap);
+}
+
+fn clear(bitmap: []u8) void {
+   for (bitmap) |_, index| {
+       bitmap[index] = 0;
+   }
 }
 
 test "fb0 exists" {
