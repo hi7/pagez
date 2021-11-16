b4134a5f3b652bcdf92ee923ddb2f839f88cce32
Hilger Baumstark <hilger.zone@gmail.com>
2021-11-16 22:18:39 +0100

    read screen resolution from file system

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,34 @@
 const std = @import("std");
 const fs = std.fs;
+const mem = std.mem;
 const Vector = std.meta.Vector;
+const expect = std.testing.expect;
+
+const Point = struct {
+    x: u16, y: u16
+};
 
 pub fn main() anyerror!void {
-    var buffer: [1366*768*4]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    var allocator = &fba.allocator;
-
-    var bitmap = try allocator.alloc(u8, 1366*768*4);
+    const res = try resolution();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+    const size: u32 = @as(u32, res.x) * @as(u32, res.y) * @as(u32, 4);
+    var bitmap = try allocator.alloc(u8, size);
     defer allocator.free(bitmap);
 
-    clear(bitmap);
-    
-    const white: Vector(4, u8) = .{ 255, 255, 255, 255 };
-    pixel(white, bitmap, 0);
-    const blue = [4]u8{ 255, 0, 0, 255 };
-    pixel(blue, bitmap, 1);
+    try draw(bitmap, res);
+}
 
+fn draw(bitmap: []u8, res: Point) anyerror!void {
+    clear(bitmap);
+
+    const white: Vector(4, u8) = .{ 255, 255, 255, 255 };
+    pixel(white, 0, 0, bitmap, res);
+    pixel(white, res.x - 1, 0, bitmap, res);
+    const blue = [4]u8{ 255, 0, 0, 255 };
+    pixel(blue, 1, 1, bitmap, res);
+    pixel(blue, res.x - 2, 1, bitmap, res);
 
     const fb = try fs.openFileAbsolute("/dev/fb0", .{ .write = true });
     defer fb.close();
@@ -24,23 +36,50 @@ pub fn main() anyerror!void {
     try flush(bitmap, fb);
 }
 
-fn pixel(color: Vector(4,u8), bitmap: []u8, pos: u16) void {
-   bitmap[pos] = color[0];
-   bitmap[pos+1] = color[1];
-   bitmap[pos+2] = color[2];
-   bitmap[pos+3] = color[3];
+const ParseError = error{
+      SeparatorNotFound,
+      NoIntegerValue,
+};
+
+fn resolution() anyerror!Point {
+    var res = try fs.openFileAbsolute("/sys/class/graphics/fb0/virtual_size", .{ .read = true });
+    defer res.close();
+
+    var buf: [15]u8 = undefined;
+    var bytes_read = try res.readAll(&buf);
+    // remove line feed at the end
+    if (!std.ascii.isDigit(buf[bytes_read])) { bytes_read -= 1; }
+    const separator = std.mem.indexOf(u8, buf[0..bytes_read], ",");
+    if (separator == null) return ParseError.SeparatorNotFound;
+    const width = std.fmt.parseInt(u16, buf[0..separator.?], 10) catch {
+        std.debug.print("{s} is no u16 value\n", .{buf[0..separator.?]});
+        return ParseError.NoIntegerValue;
+    };
+    const height = std.fmt.parseInt(u16, buf[(separator.?+1)..bytes_read], 10) catch {
+        std.debug.print("{s} is no u16 value\n", .{buf[(separator.?+1)..bytes_read]});
+        return ParseError.NoIntegerValue;
+    };
+    return Point{ .x = width, .y = height };
+}
+
+fn pixel(color: Vector(4, u8), x: u16, y: u16, bitmap: []u8, res: Point) void {
+    const pos = (res.x * y + x) * 4;
+    bitmap[pos] = color[0];
+    bitmap[pos + 1] = color[1];
+    bitmap[pos + 2] = color[2];
+    bitmap[pos + 3] = color[3];
 }
 
 fn flush(bitmap: []u8, fb: fs.File) fs.File.PWriteError!void {
-   _ = try fb.write(bitmap);
+    _ = try fb.write(bitmap);
 }
 
 fn clear(bitmap: []u8) void {
-   for (bitmap) |_, index| {
-       bitmap[index] = 0;
-   }
+    for (bitmap) |_, index| {
+        bitmap[index] = 0;
+    }
 }
 
 test "fb0 exists" {
-     try fs.accessAbsolute("/dev/fb0", .{ .write = true });
+    try fs.accessAbsolute("/dev/fb0", .{ .write = true });
 }
