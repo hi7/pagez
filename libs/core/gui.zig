246bf4ac4065abea91bc1d0361449ca69e567587
Hilger Baumstark <hilger.zone@gmail.com>
2021-11-19 15:55:10 +0100

    rename lib filez to pagez

--- libs/core/gui.zig
+++ libs/core/gui.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
-const filez = @import("filez.zig");
+const pagez = @import("pagez.zig");
 const Vector = std.meta.Vector;
-const Point = filez.Point;
+const Point = pagez.Point;
 
 pub const white: Vector(4, u8) = Vector(4, u8){ 255, 255, 255, 255 };
 pub const yellow: Vector(4, u8) = Vector(4, u8){ 0, 255, 255, 255 };
@@ -12,28 +12,28 @@ pub const magenta: Vector(4, u8) = Vector(4, u8){ 255, 0, 255, 255 };
 pub const cyan: Vector(4, u8) = Vector(4, u8){ 255, 255, 0, 255 };
 
 fn calcPos(x: u16, y: u16) u32 {
-   return (@as(u32, filez.display_size.x) * @as(u32, y) + @as(u32, x)) *% @as(u32, 4);
+   return (@as(u32, pagez.display_size.x) * @as(u32, y) + @as(u32, x)) *% @as(u32, 4);
 }
 
 pub fn pixel(color: Vector(4, u8), x: u16, y: u16) void {
     const offset = calcPos(x, y);
-    filez.bitmap[offset] = color[0];
-    filez.bitmap[offset + 1] = color[1];
-    filez.bitmap[offset + 2] = color[2];
-    filez.bitmap[offset + 3] = color[3];
+    pagez.bitmap[offset] = color[0];
+    pagez.bitmap[offset + 1] = color[1];
+    pagez.bitmap[offset + 2] = color[2];
+    pagez.bitmap[offset + 3] = color[3];
 }
 
 pub fn box(color: Vector(4, u8), pos: Point, size: Point) void {
     const offset = calcPos(pos.x, pos.y);
     var dx: u16 = 0;
     var dy: u16 = 0;
-    while (dy < filez.display_size.y) : (dy += 1) {
+    while (dy < pagez.display_size.y) : (dy += 1) {
         const yoffset:u32 = dy * size.x * @as(u32, 4);
         while (dx < size.x*4) : (dx += 4) {
-            filez.bitmap[offset + yoffset + dx] = color[0];
-            filez.bitmap[offset + yoffset + dx + 1] = color[1];
-            filez.bitmap[offset + yoffset + dx + 2] = color[2];
-            filez.bitmap[offset + yoffset + dx + 3] = color[3];
+            pagez.bitmap[offset + yoffset + dx] = color[0];
+            pagez.bitmap[offset + yoffset + dx + 1] = color[1];
+            pagez.bitmap[offset + yoffset + dx + 2] = color[2];
+            pagez.bitmap[offset + yoffset + dx + 3] = color[3];
         }
         dx = 0;
     }
--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,34 @@
 const std = @import("std");
-const filez = @import("filez");
+const pagez = @import("pagez");
 const gui = @import("gui");
 const fs = std.fs;
 const mem = std.mem;
-const Point = filez.Point;
+const Point = pagez.Point;
 const expect = std.testing.expect;
 
 pub fn main() anyerror!void {
-    try filez.init();
+    try pagez.init();
     try draw();
 
-    var m = try filez.readMouse();
-    var mp = Point{ .x = filez.display_size.x / 2, .y = filez.display_size.y / 2 };
+    var m = try pagez.readMouse();
+    var mp = Point{ .x = pagez.display_size.x / 2, .y = pagez.display_size.y / 2 };
     gui.box(gui.white, mp, Point{ .x = 4, .y = 4});
     while (!m.lmb) {
         mp.x = @intCast(u16, (@intCast(i16, mp.x) + @intCast(i16, m.dx)));
         mp.y = @intCast(u16, (@intCast(i16, mp.y) + @intCast(i16, m.dy) * -1));
         gui.box(gui.yellow, mp, Point{ .x = 8, .y = 8});
-        try filez.flush();
-        m = try filez.readMouse();
+        try pagez.flush();
+        m = try pagez.readMouse();
     }
-    filez.exit();
+    pagez.exit();
 }
 
 fn draw() anyerror!void {
-    filez.clear();
+    pagez.clear();
     gui.box(gui.white, Point{ .x = 0, .y = 0},  Point{ .x = 8, .y = 8});
-    gui.box(gui.white, Point{ .x = filez.display_size.x-9, .y = 0},  Point{ .x = 8, .y = 8});
+    gui.box(gui.white, Point{ .x = pagez.display_size.x-9, .y = 0},  Point{ .x = 8, .y = 8});
     gui.box(gui.magenta, Point{ .x = 8, .y = 5},  Point{ .x = 3, .y = 3});
-    gui.box(gui.magenta, Point{ .x = filez.display_size.x-12, .y = 5},  Point{ .x = 3, .y = 3});
+    gui.box(gui.magenta, Point{ .x = pagez.display_size.x-12, .y = 5},  Point{ .x = 3, .y = 3});
 
-    try filez.flush();
+    try pagez.flush();
 }
