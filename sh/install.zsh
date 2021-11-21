539ffeeb2d65069c0b60ed20470c1838c7477cf5
Hilger Baumstark <hilger.zone@gmail.com>
2021-11-21 18:09:21 +0100

    remove archive file

--- sh/install.zsh
+++ sh/install.zsh
@@ -4,8 +4,8 @@ if [[ $1 == $V ]]; then echo "install pagez (verbose mode)"; fi
 
 INSTDIR=~/tools
 if [ ! -d $INSTDIR ]; then
-	mkdir $INSTDIR;
 	if [[ $1 == $V ]]; then echo "mkdir $INSTDIR"; fi
+	mkdir $INSTDIR;
 fi
 ZIGVERSION=0.8.1
 ZIGDLPATH="https://ziglang.org/download/$ZIGVERSION/"
@@ -14,26 +14,28 @@ ZIGBIN=zig
 ZIGFILE=${ZIGBIN}-${OSTYPE%%-*}-$CPUTYPE-$ZIGVERSION
 if [ ! -d $INSTDIR/$ZIGFILE ]; then
 	if [ ! -f $INSTDIR/$ZIGFILE$ZIGEXT ]; then
-		wget -P $INSTDIR ${ZIGDLPATH}$ZIGFILE$ZIGEXT;
 		if [[ $1 == $V ]]; then echo wget -P $INSTDIR ${ZIGDLPATH}$ZIGFILE$ZIGEXT $INSTDIR; fi
+		wget -P $INSTDIR ${ZIGDLPATH}$ZIGFILE$ZIGEXT;
 	fi
 	if [[ $1 == $V ]]; then echo "tar xf $INSTDIR/$ZIGFILE$ZIGEXT -C $INSTDIR"; fi
 	tar xf $INSTDIR/$ZIGFILE$ZIGEXT -C $INSTDIR;
+	if [[ $1 == $V ]]; then echo "rm $INSTDIR/$ZIGFILE$ZIGEXT"; fi
+	rm $INSTDIR/$ZIGFILE$ZIGEXT;
 fi
 
 USERBIN=~/bin
 if [ ! -d $USERBIN ]; then
-	mkdir $USERBIN;
 	if [[ $1 == $V ]]; then echo "mkdir $USERBIN"; fi
+	mkdir $USERBIN;
 fi
 
 if [ ! -L $USERBIN/$ZIGBIN ]; then
-	ln -s $INSTDIR/$ZIGFILE/$ZIGBIN $USERBIN/$ZIGBIN;
 	if [[ $1 == $V ]]; then echo "ln -s $INSTDIR/$ZIGFILE/$ZIGBIN $USERBIN/$ZIGBIN"; fi
+	ln -s $INSTDIR/$ZIGFILE/$ZIGBIN $USERBIN/$ZIGBIN;
 fi
 
 if [ -d "$HOME/bin" ]; then
-	PATH="$HOME/bin:$PATH";
 	if [[ $1 == $V ]]; then echo "PATH=$HOME/bin:$PATH"; fi
+	PATH="$HOME/bin:$PATH";
 fi
 
